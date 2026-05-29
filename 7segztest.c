774c50725268cc9483c574b9623e0dc7dd5d66a1
MANEESHGULLA <maneesh2045@gmail.com>
2026-05-29 22:09:44 +0530

    Update 7segztest.c

--- 7segztest.c
+++ 7segztest.c
@@ -1,120 +1,90 @@
-// 20 to 0
-
-#include <reg51.h>
-
-#define seven_segment_data P2
-
-sbit DISP1 = P0^1;   // Tens digit
-sbit DISP2 = P0^0;   // Units digit
-
-void delay_ms(unsigned int time);
-
-void main()
-{
-unsigned char tens = 2, units = 0;
-
-unsigned char bcd_code[] =
-{0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F};
-
-while(1)
-{
-
-DISP1 = 0;
-seven_segment_data = bcd_code[tens];
-delay_ms(2);
-DISP1 = 1;
-
-DISP2 = 0;
-seven_segment_data = bcd_code[units];
-delay_ms(2);
-DISP2 = 1;
-
-if(units == 0)
-{
-units = 9;
-tens--;
-}
-else
-{
-units--;
-}
-
-if(tens == 255)
-{
-tens = 2;
-units = 0;
-}
-
-}
-}
-
-void delay_ms(unsigned int time)
-{
-unsigned int i,j;
-for(i=0;i<time;i++)
-for(j=0;j<500;j++);
-}
-
-
-
-
-
-
-
-
-
-
 // 0 to 20
 #include <reg51.h>
 
-#define seven_segment_data P2
+#define seven_segment_data P1
 
-sbit DISP1 = P0^1;
-sbit DISP2 = P0^0;
+sbit disp1_sel = P0^3;
+sbit disp2_sel = P0^2;
+sbit disp3_sel = P0^1;
+sbit disp4_sel = P0^0;
 
-void delay_ms(unsigned int time);
+void delay_ms(unsigned int);
 
-void main()
+void main(void)
 {
-unsigned char tens = 0, units = 0;
+    unsigned char count0 = 0;   // Delay counter
+    unsigned char count1 = 0;   // Units digit
+    unsigned char count2 = 0;   // Tens digit
 
-unsigned char bcd_code[] =
-{0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F};
+    unsigned char bcd_codes[] =
+    {
+        0x3F, 0x06, 0x5B, 0x4F, 0x66,
+        0x6D, 0x7D, 0x07, 0x7F, 0x6F
+    };
 
-while(1)
-{
+    while(1)
+    {
+        /* Units Digit */
+        disp1_sel = 0;
+        seven_segment_data = bcd_codes[count1];
+        delay_ms(2);
+        disp1_sel = 1;
 
-DISP1 = 0;
-seven_segment_data = bcd_code[tens];
-delay_ms(2);
-DISP1 = 1;
+        /* Tens Digit */
+        disp2_sel = 0;
+        seven_segment_data = bcd_codes[count2];
+        delay_ms(2);
+        disp2_sel = 1;
 
-DISP2 = 0;
-seven_segment_data = bcd_code[units];
-delay_ms(2);
-DISP2 = 1;
+        /* Hundreds Digit = 0 */
+        disp3_sel = 0;
+        seven_segment_data = bcd_codes[0];
+        delay_ms(2);
+        disp3_sel = 1;
 
-units++;
+        /* Thousands Digit = 0 */
+        disp4_sel = 0;
+        seven_segment_data = bcd_codes[0];
+        delay_ms(2);
+        disp4_sel = 1;
 
-if(units > 9)
-{
-units = 0;
-tens++;
+        count0++;
+
+        if(count0 > 25)
+        {
+            count0 = 0;
+            count1++;
+        }
+
+        if(count1 > 9)
+        {
+            count1 = 0;
+            count2++;
+        }
+
+        /* Stop at 20 */
+
+        if(count2 == 2 && count1 == 1)
+        {
+            count1 = 0;
+            while(1);
+        }
+    }
 }
 
-if(tens == 2 && units == 1)
+void delay_ms(unsigned int i)
 {
-tens = 0;
-units = 0;
+    unsigned int j;
+
+    while(i-- > 0)
+    {
+        for(j = 0; j < 500; j++)
+        {
+            ;
+        }
+    }
 }
 
-}
-}
 
-void delay_ms(unsigned int time)
-{
-unsigned int i,j;
-for(i=0;i<time;i++)
-for(j=0;j<500;j++);
-}
 
+//==========================================================================================
