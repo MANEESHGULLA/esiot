daf4a9c873a1a7e04342c20c190cf5d7e88e168c
MANEESHGULLA <maneesh2045@gmail.com>
2026-05-29 21:23:41 +0530

    Update 7segment.c

--- 7segment.c
+++ 7segment.c
@@ -1,86 +1,93 @@
 #include <reg51.h>
 
-#define seven_segment_data P2
+#define seven_segment_data P1
 
-sbit DISP1_SEL = P0^3;
-sbit DISP2_SEL = P0^2;
-sbit DISP3_SEL = P0^1;
-sbit DISP4_SEL = P0^0;
-sbit led_back_light = P0^2;
+sbit disp1_sel = P0^3;
+sbit disp2_sel = P0^2;
+sbit disp3_sel = P0^1;
+sbit disp4_sel = P0^0;
+sbit lcd_back_light = P0^7;
 
 void delay_ms(unsigned int);
 
 void main(void)
 {
-    unsigned char count0=0,count1=0,count2=0,count3=0,count4=0;
+    unsigned char count0 = 0;
+    unsigned char count1 = 0;
+    unsigned char count2 = 0;
+    unsigned char count3 = 0;
+    unsigned char count4 = 0;
 
-    unsigned char bcd_code[]=
-    {0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F};
+    unsigned char bcd_codes[] =
+    {
+        0x3F, 0x06, 0x5B, 0x4F, 0x66,
+        0x6D, 0x7D, 0x07, 0x7F, 0x6F
+    };
 
     while(1)
     {
-        DISP1_SEL = 0;
-        seven_segment_data = bcd_code[count1];
+        disp1_sel = 0;
+        seven_segment_data = bcd_codes[count1];
         delay_ms(2);
-        DISP1_SEL = 1;
+        disp1_sel = 1;
 
-        DISP2_SEL = 0;
-        seven_segment_data = bcd_code[count2];
+        disp2_sel = 0;
+        seven_segment_data = bcd_codes[count2];
         delay_ms(2);
-        DISP2_SEL = 1;
+        disp2_sel = 1;
 
-        DISP3_SEL = 0;
-        seven_segment_data = bcd_code[count3];
+        disp3_sel = 0;
+        seven_segment_data = bcd_codes[count3];
         delay_ms(2);
-        DISP3_SEL = 1;
+        disp3_sel = 1;
 
-        DISP4_SEL = 0;
-        seven_segment_data = bcd_code[count4];
+        disp4_sel = 0;
+        seven_segment_data = bcd_codes[count4];
         delay_ms(2);
-        DISP4_SEL = 1;
+        disp4_sel = 1;
 
         count0++;
 
-        if(count0 >= 9)
+        if(count0 > 25)
         {
             count0 = 0;
             count1++;
         }
 
-        if(count1 >= 9)
+        if(count1 > 9)
         {
             count1 = 0;
             count2++;
         }
 
-        if(count2 >= 9)
+        if(count2 > 9)
         {
             count2 = 0;
             count3++;
         }
 
-        if(count3 >= 9)
+        if(count3 > 9)
         {
             count3 = 0;
             count4++;
         }
 
-        if(count4 >= 9)
+        if(count4 > 9)
         {
             count4 = 0;
         }
     }
 }
 
-
-void delay_ms(unsigned int time)
+void delay_ms(unsigned int i)
 {
-    unsigned int i, j;
+    unsigned int j;
 
-    for(i = 0; i < time; i++)
+    while(i-- > 0)
     {
         for(j = 0; j < 500; j++)
         {
+            ;
         }
     }
 }
